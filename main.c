1346ac88c7e0f0a64f6602bc12a16a4a70179780
InconsolableCellist <sadfklj@sdlkfj.com>
2021-08-08 23:05:41 -0600

    Updates

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_render.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
-const int SCREEN_WIDTH = 480;
-const int SCREEN_HEIGHT = 640;
+//#define DATA_FILE "data.dat"
+#define DATA_FILE "/dev/ttyACM3"
+
+const int SCREEN_WIDTH = 3100;
+const int SCREEN_HEIGHT = 2000;
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gHelloWorld = NULL;
 SDL_Surface* gStretchedSurface = NULL;
+
+int gDataFile = 0;
+
 
 int init();
 int loadMedia();
@@ -18,6 +27,13 @@ SDL_Surface* loadSurface(char* path);
 
 int main(int argc, char* argv[]) {
     int should_quit = 0;
+    int x = 0;
+    int y = 0;
+    uint16_t data = 0;
+    double syncDuration = 0;
+    double pixelIntensity = 0;
+    uint8_t scanMode = 0;
+    int status = 0;
     SDL_Event e;
     SDL_Rect stretchRect;
 
@@ -34,8 +50,6 @@ int main(int argc, char* argv[]) {
     stretchRect.w = SCREEN_WIDTH;
     stretchRect.h = SCREEN_HEIGHT;
 
-    int x = 0;
-    int y = 0;
     while(!should_quit) {
         while(SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
@@ -43,13 +57,49 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+        status = read(gDataFile, &data, 2);
+        if (status <= 0) {
+            printf("Fatal error when trying to read data! Status: %d. Errno: %d\n", status, errno);
+            continue;
+        }
+        while (data == 0xFEFE) {
+            read(gDataFile, &data, 2);
+            syncDuration = data/16;
+            read(gDataFile, &data, 2);
+            syncDuration += ((double)data) / 1000000;
+            read(gDataFile, &data, 2);
+            scanMode = data;
+            if (syncDuration >= 0.5) {
+                x = 0;
+                y = 0;
+            } else {
+                if (!(y%4)) {
+                    SDL_RenderPresent(gRenderer);
+                    printf("scanMode: %d", scanMode);
+                }
+                x = 0;
+                y += 5;
+            }
+
+            read(gDataFile, &data, 2);
+        }
+
+        pixelIntensity = floor( ((double)data) /1024*255);
+        if (pixelIntensity > 255) {
+            pixelIntensity = 255;
+        }
+
+        SDL_SetRenderDrawColor(gRenderer, pixelIntensity, pixelIntensity, pixelIntensity, 255);
         SDL_RenderDrawPoint(gRenderer, x, y);
+        SDL_RenderDrawPoint(gRenderer, x, y+1);
+        SDL_RenderDrawPoint(gRenderer, x, y+2);
+        SDL_RenderDrawPoint(gRenderer, x, y+3);
+        SDL_RenderDrawPoint(gRenderer, x, y+4);
         x += 1;
         if (x >= SCREEN_WIDTH) {
             x = 0;
             y += 1;
-            SDL_RenderPresent(gRenderer);
+//            SDL_RenderPresent(gRenderer);
         }
         if (y >= SCREEN_HEIGHT) {
             y = 0;
@@ -65,26 +115,26 @@ int main(int argc, char* argv[]) {
 }
 
 int init() {
-    int succ = 1;
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL initialization failure! %d\n", SDL_GetError());
-        succ = 0;
-    } else {
+        return 0;
+    }
 //        gWindow = SDL_CreateWindow("S-2500 Image Parser", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
 //                                   SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &gWindow, &gRenderer);
-        SDL_SetWindowTitle(gWindow, "S-2500 Image Parser");
-        if (!gWindow) {
-            printf("Window couldn't be created! %s\n", SDL_GetError());
-            succ = 0;
-        } else {
-            gScreenSurface = SDL_GetWindowSurface(gWindow);
-            SDL_RenderClear(gRenderer);
-        }
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &gWindow, &gRenderer);
+    SDL_SetWindowTitle(gWindow, "S-2500 Image Parser");
+    if (!gWindow) {
+        printf("Window couldn't be created! %s\n", SDL_GetError());
+    }
+    gScreenSurface = SDL_GetWindowSurface(gWindow);
+    SDL_RenderClear(gRenderer);
+    gDataFile = open(DATA_FILE, O_RDONLY);
+    if (gDataFile == -1) {
+        printf("Unable to open data file %s!\n", DATA_FILE);
+        return 0;
     }
 
-    return succ;
+    return 1;
 }
 
 int loadMedia() {
@@ -92,7 +142,7 @@ int loadMedia() {
 
     gHelloWorld = SDL_LoadBMP("hello.bmp");
     if (!gHelloWorld) {
-        printf("Failed to load media! %s", SDL_GetError());
+        printf("Failed to load media! %s\n", SDL_GetError());
         succ = 0;
     }
 
