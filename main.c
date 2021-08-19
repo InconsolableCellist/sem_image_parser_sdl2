3eb69cbf3c562d97f189022c722c2c45e63c7c59
InconsolableCellist <sadfklj@sdlkfj.com>
2021-08-19 14:01:05 -0600

    Stretched rendering

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 #include <unistd.h>
 #include <errno.h>
 
-//#define DATA_FILE "data.dat"
-#define DATA_FILE "/dev/ttyACM1"
+#define DATA_FILE "../data.dat"
+//#define DATA_FILE "/dev/ttyACM1"
 
 #define BUF_SIZE 1024
 
 const int SCREEN_WIDTH = 768;
 const int SCREEN_HEIGHT = 1024;
+const int SOURCE_WIDTH = 768;
+const int SOURCE_HEIGHT = 1024;
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
-SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gHelloWorld = NULL;
 SDL_Surface* gStretchedSurface = NULL;
 SDL_Texture* gTexture = NULL;
@@ -60,19 +61,15 @@ int main(int argc, char* argv[]) {
         dataBuffer[i] = 0x0;
     }
 
-    gPixels = malloc((sizeof(uint32_t) * SCREEN_WIDTH * SCREEN_HEIGHT));
-    memset(gPixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
-//    for (uint16_t i=0; i<SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
-//        gPixels[i] =
-//    }
+    gPixels = malloc((sizeof(uint32_t) * SOURCE_WIDTH * SOURCE_HEIGHT));
+    memset(gPixels, 0, SOURCE_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
 
-//    gStretchedSurface = loadSurface("hello.bmp");
     stretchRect.x = 0;
     stretchRect.y = 0;
     stretchRect.w = SCREEN_WIDTH;
     stretchRect.h = SCREEN_HEIGHT;
 
-    gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+    gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SOURCE_WIDTH, SOURCE_HEIGHT);
 
     while(!should_quit) {
         while(SDL_PollEvent(&e)) {
@@ -89,8 +86,7 @@ int main(int argc, char* argv[]) {
             should_quit = 1;
         }
 
-//        SDL_RenderPresent(gRenderer);
-        SDL_UpdateTexture(gTexture, NULL, gPixels, SCREEN_WIDTH * sizeof(uint32_t));
+        SDL_UpdateTexture(gTexture, NULL, gPixels, SOURCE_WIDTH * sizeof(uint32_t));
         for (uint16_t i=0; i<(status/sizeof(uint16_t)); i++) {
             while (dataBuffer[i] == 0xFEFA) {
                 syncDuration = dataBuffer[++i] / 16;
@@ -112,20 +108,19 @@ int main(int argc, char* argv[]) {
                     x = 0;
                     y = 0;
                     SDL_RenderClear(gRenderer);
-                    SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+                    SDL_RenderCopy(gRenderer, gTexture, NULL, &stretchRect);
                     SDL_RenderPresent(gRenderer);
                 } else {
                     if (!(y%1)) {
                         printf("scanMode: %d\n\tpulse duration: %f\n\tframe duration: %f\n", scanMode, syncDuration, frameDuration);
                         printf("\tsyncAverage: %f\n\tmaxSync: %f\n\tminSync: %f\n", syncAverage, maxSync, minSync);
                         SDL_RenderClear(gRenderer);
-                        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+                        SDL_RenderCopy(gRenderer, gTexture, NULL, &stretchRect);
                         SDL_RenderPresent(gRenderer);
                     }
-//                    SDL_RenderPresent(gRenderer);
 //                    printf("x pulse at x = %d\n\tpulse duration: %f\n\tframe duration %g\n\tscanmode: %d\n", x, syncDuration, frameDuration, scanMode);
                     x = 0;
-                    y += 2;
+                    y += 1;
                 }
                 syncNum += 1;
                 syncAverage += syncDuration/syncNum;
@@ -154,36 +149,19 @@ int main(int argc, char* argv[]) {
 //                pixelIntensity = 100;
 //            }
 
-            if (x < SCREEN_WIDTH) {
-//                SDL_SetRenderDrawColor(gRenderer, pixelIntensity, pixelIntensity, pixelIntensity, 255);
-//                SDL_RenderDrawPoint(gRenderer, x, y);
-//                SDL_RenderDrawPoint(gRenderer, x, y+1);
-//                SDL_RenderDrawPoint(gRenderer, x, y+2);
-//                SDL_RenderDrawPoint(gRenderer, x, y+3);
-//                SDL_RenderDrawPoint(gRenderer, x, y+4);
+            if (x < SOURCE_WIDTH) {
                 uint32_t val = ((uint8_t)pixelIntensity) << 24;
                 val |= ((uint8_t)pixelIntensity) << 16;
                 val |= ((uint8_t)pixelIntensity) << 8;
                 val |= ((uint8_t)pixelIntensity);
-                gPixels[y * SCREEN_WIDTH + x] = val;
-                if (y < SCREEN_HEIGHT - 4) {
-                    gPixels[(y+1) * SCREEN_WIDTH + x] = val;
-//                    gPixels[(y+2) * SCREEN_WIDTH + x] = val;
-//                    gPixels[(y+3) * SCREEN_WIDTH + x] = val;
-//                    gPixels[(y+4) * SCREEN_WIDTH + x] = val;
-                }
+                gPixels[y * SOURCE_WIDTH + x] = val;
             }
             x += 1;
-            if (y >= SCREEN_HEIGHT) {
+            if (y >= SOURCE_HEIGHT) {
                 y = 0;
                 printf("frame overflow\n");
             }
         }
-
-
-//            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-//        SDL_BlitScaled(gStretchedSurface, NULL, gScreenSurface, &stretchRect);
-//        SDL_UpdateWindowSurface(gWindow);
     }
 
     quit();
@@ -195,14 +173,12 @@ int init() {
         printf("SDL initialization failure! %d\n", SDL_GetError());
         return 0;
     }
-//        gWindow = SDL_CreateWindow("S-2500 Image Parser", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-//                                   SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &gWindow, &gRenderer);
     SDL_SetWindowTitle(gWindow, "S-2500 Image Parser");
     if (!gWindow) {
         printf("Window couldn't be created! %s\n", SDL_GetError());
     }
-    gScreenSurface = SDL_GetWindowSurface(gWindow);
+//    gScreenSurface = SDL_GetWindowSurface(gWindow);
     SDL_RenderClear(gRenderer);
     gDataFile = open(DATA_FILE, O_RDONLY);
     if (gDataFile == -1) {
@@ -223,23 +199,6 @@ int loadMedia() {
     }
 
     return succ;
-}
-
-SDL_Surface* loadSurface(char* path) {
-    SDL_Surface* optimizedSurface = NULL;
-
-    SDL_Surface* loadedSurface = SDL_LoadBMP(path);
-    if (!loadedSurface) {
-        printf("Unable to load image %s! Erorr: %s\n", path, SDL_GetError());
-    } else {
-        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
-        if (!optimizedSurface) {
-            printf("Unable to optimize image %s! Error: %s\n", path, SDL_GetError());
-        }
-        SDL_FreeSurface(loadedSurface);
-    }
-
-    return optimizedSurface;
 }
 
 void quit() {
